c6449d199de57b6f9a88accaf86e4cb9fe2c7e84
baalthazard <etienne.vill@gmail.com>
2014-04-21 21:07:37 +0200

    Model - Document / Utilisateur / Exigences / Projet pas de clés étrangères

--- Project_21/EXiT/models.py
+++ Project_21/EXiT/models.py
@@ -1,3 +1,65 @@
-from django.db import models
+# -*- coding: latin-1 -*-
 
-# Create your models here.
+from django.db import models
+from django.core.exceptions import ValidationError
+import datetime
+
+class Projet(models.Model):
+    """
+    Objet qui rassemble toutes les informations relatives à un Projet
+    C'est à dire :
+                    - id : identifiant unique (généré autaomatiquement par Django)
+    				- date_debut : la date de lancement du projet
+    				- nom : le nom du Projet
+    				- descriptif : description du projet
+    				- en_cours : si le projet est en cours ou non
+    				- nom_client : le nom du client
+    				- responsable : l'utilisateur responsable
+    """
+    date_debut = models.DateField('Date publiee',auto_now=True,help_text="la date à laquelle le projet a debute")
+    nom = models.CharField(max_length=400,help_text="nom du projet")
+    descriptif = models.CharField(max_length=8000,help_text="informations relatives au document")
+    en_cours = models.BooleanField(default=True, help_text="état du projet, si il est en cours ou non")
+    nom_client = models.CharField(max_length=400,help_text="le nom du client princiapl du projet")
+    responsable = models.utilisateur(help_text="le responsable du projet")
+
+class Document(models.Model):
+    """
+    Objet qui rasemble les informations d'un document utilise au sein de l'application.
+    C'est à dire :
+                 - id : identifiant unique (généré autaomatiquement par Django)
+    			 - date_publiee : la date correspondant a son entree dans la base de donnee
+    			 - proprietaire : l'utilisateur qui a rentre le document au sein du systeme
+    			 - commentaire : les informations relatives au documents saisie par l'utilisateur
+
+    """
+    url = models.URLField("URL",unique=True)
+    date_publiee = models.DateField('Date publiee',auto_now=True,help_text="La date a laquelle le document a ete integre au systeme")
+    proprietaire = models.utilisateur(help_text="personne ayant saisie le document au sein de l'application")
+    descriptif = models.CharField(max_length=8000,help_text="informations relatives au document")
+    commentraire = models.CharField(max_length=8000,help_text="commentaires sur document")
+
+class Utilisateur(models.Model):
+     """
+     Objet qui rassemble les informations liées à un utilisateur.
+     C'est à dire :
+                  - id : identifiant unique (généré autaomatiquement par Django)
+                  - nom : le nom de la personne
+                  - prenom : le nom de la personne
+                  - informations : des informations relatives à la personnes
+     """
+
+     nom = models.CharField(max_length=400,help_text="nom de l'utilisateur")
+     prenom = models.CharField(max_length=400,help_text="prenom de l'utilisateur")
+     informations = models.CharField(max_length=400,help_text="informations relatives a l'utilisateur")
+
+class Exigence(models.Model):
+    """
+    Objet qui rassemble toutes les informations relatives à une éxigences.
+    C'est à dire :
+                 - id : identifiant unique (généré autaomatiquement par Django)
+                 - description : la description de l'éxigence
+                 - commentaire : commentaire sur l'exigence
+    """
+    id_unique = models.IntegerField(unique=True)
+    commentraire = models.CharField(max_length=8000,help_text="informations relatives a l'exigence")
