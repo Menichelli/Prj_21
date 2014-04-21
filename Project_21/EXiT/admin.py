95f28c01703cdb6b627615d6a932cc5bdeb23f07
Christophe Menichelli <menichelli.c@gmail.com>
2014-04-21 23:07:59 +0200

    Interface admin

--- Project_21/EXiT/admin.py
+++ Project_21/EXiT/admin.py
@@ -1,3 +1,15 @@
+from EXiT.models import *
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.models import User
+#from django.contrib.sites.models import Site
+from django.contrib.auth.models import Group
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+#admin.site.unregister(Site)
+
+admin.site.register(Projet)
+admin.site.register(Document)
+admin.site.register(Exigence)
+admin.site.register(Utilisateur)
