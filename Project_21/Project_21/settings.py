91b5817c0ce08c6ecbf5202ef5d8ba90064707f2
Christophe Menichelli <menichelli.c@gmail.com>
2014-04-21 20:40:53 +0200

    Start project

--- Project_21/Project_21/settings.py
+++ Project_21/Project_21/settings.py
@@ -13,7 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 ADMINS = (
-    ('admin','menichelli.c@gmail.com'), #ca faudra le changer :0
+    ('admin','admin@admin.fr'), #a changer
 )
 
 # Quick-start development settings - unsuitable for production
